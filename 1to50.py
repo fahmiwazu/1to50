843b948ab4c2bdcda97af19e6ce296b8905a505d
fahmi <fahmiwazu@gmail.com>
2024-07-27 16:32:07 +0700

    feat: update project architechture

--- 1to50.py
+++ 1to50.py
@@ -1,6 +1,5 @@
 import time
 import logging
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -19,32 +18,44 @@ serv_obj = Service("D:\\Work\\webdriver\\chromedriver-win64\\chromedriver.exe")
 # Opening Chrome Browser
 driver = webdriver.Chrome(service=serv_obj)
 
-# Maximize Windows
-driver.maximize_window()
+def Main():
 
-# Insert Puzzle URI
-driver.get("https://zzzscore.com/1to50/en/")
+    NavitageURL();
+    Solver();
+    GetRecord();
 
-# Automation Algorithm
-a=1
-while(a!=51):
-    driver.find_element(By.XPATH, "//div[normalize-space()='"+str(a)+"']//span[@class='box']").click()
-    print("number "+str(a)+" are clicked")
-    if(a%6==0):
-        time.sleep(1)
+    # Closing Browser
+    driver.close()
 
-    a=a+1
+def NavitageURL():
 
-# Complete Puzzle
-print("Quiz are completed")
+    # Maximize Windows
+    driver.maximize_window()
 
-# Retrieve Time Record
-time_record = driver.find_element(By.XPATH, "(//div[@id='time'])[1]")
-record = time_record.text
+    # Insert Puzzle URI
+    driver.get("https://zzzscore.com/1to50/en/")
 
-# Display time Record
-print("Congratulation! your record are "+ record+ " seconds")
-time.sleep(10)
+def Solver():
+    # Automation Algorithm
+    obj_count=1
+    while(obj_count!=51):
+        driver.find_element(By.XPATH, "//div[normalize-space()='"+str(obj_count)+"']//span[@class='box']").click()
+        print("number "+str(obj_count)+" are clicked")
+        if(obj_count%51==0):
+            time.sleep(1)
+        obj_count=obj_count+1
 
-# Closing Browser
-driver.close()
+    # Complete Puzzle
+    print("Quiz are completed")
+
+def GetRecord():
+    # Retrieve Time Record
+    time_record = driver.find_element(By.XPATH, "(//div[@id='time'])[1]")
+    record = time_record.text
+
+    # Display time Record
+    print("Congratulation! your record are "+ record+ " seconds")
+    time.sleep(10)
+
+
+Main();
